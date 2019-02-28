fcd3511e1a06a4de164d6020e86f0607210070e3
Wellvergton Marques <wmc.jp@outlook.com>
2019-02-27 23:48:16 -0300

    Improvements on the navigation, included collapsible for side nav, some minor changes on clours

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -39,6 +39,10 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
 
 // document.addEventListener('DOMContentLoaded', function() {
 //   var elems = document.querySelectorAll('.btn-floating');
